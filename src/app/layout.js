614ab480ab31d61b51efa7b4b6615580b1fce757
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-12-26 12:41:03 +0200

    start

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google'
 
 import './globals.css'
-import Header from "./components/Header";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +14,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Header/>
-
       <div className={'container'}>{children}</div>
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,9 @@
-import Link from "next/link";
-
-
-async function fetchData() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const result = await res.json();
-    return result;
-}
-
 
 export default async function Home() {
-    const posts = await fetchData();
-
 
     return (
         <main>
-            {posts &&
-            posts.map(obj =>
-                <div key={obj.id} className={'post'}>
-                    <h2>{obj.title}</h2>
-                    <p>{obj.body}</p>
-                    <Link href={'/post/' + obj.id}>Details</Link>
-                </div>)
-            }
+            Next.js
         </main>
     )
 }
